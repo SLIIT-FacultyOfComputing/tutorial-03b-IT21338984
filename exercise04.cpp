562ea2a5e7758744d3cc73570eb51ab773b6a57a
IT21338984 <99257639+IT21338984@users.noreply.github.com>
2022-03-04 07:42:00 +0000

    I changed programs to function well.

--- exercise04.cpp
+++ exercise04.cpp
@@ -2,20 +2,26 @@
 using namespace std;
 
 void print(int len, int wth);
-void input(int len, int wth);
+void input(int &length, int &width);
 
 // Do not change the main() function
-int main() {
-   int length = 10, width = 5;
-   input(length, width);
-   print(length, width);
-   return 0;
+int main() 
+{
+  int length = 10, width = 5;
+  input(length, width);
+  print(length, width);
+  return 0;
 }
 
 // Do not change the print() function
-void print(int len, int wth) {
-   cout << "Length : " << len 
-        << ", Width  : " << wth << endl;
+void print(int len, int wth){
+   cout << "Length : " << len << ", Width  : " << wth << endl;
 }
 
 // Implement the Input Function here
+void input(int &length, int &width){
+  cout<<"Length: ";
+  cin>>length;
+  cout<<"Width: " ;
+  cin>>width;
+}
